01c849c3fc6ae194021db1630fd7f26a54395b7b
IsaiahAim <halmitonoluwatobi@gmail.com>
2023-09-22 22:19:04 +0100

    fixed

--- app/buccav21/settings.py
+++ app/buccav21/settings.py
@@ -24,10 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "django-insecure-$__-(#=1o@=tp1&zh4#s@9736vtdh#=r+l659&4im+@m64wy(i"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # DEBUG = False
 
 ALLOWED_HOSTS = ['*']
